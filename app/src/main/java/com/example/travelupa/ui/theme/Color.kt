efc6c18c993cad6222818b93eeb75cae81b4a9b0
Alden Prabaswara <aldn_prbs@student.ub.ac.id>
2025-09-06 18:09:54 +0700

    Initial commit

--- app/src/main/java/com/example/travelupa/ui/theme/Color.kt
+++ app/src/main/java/com/example/travelupa/ui/theme/Color.kt
@@ -4,6 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val DarkBlue = Color(0xFF0D1B2A)
 val SpaceCadet = Color(0xFF1B263B)
-val ShadowBlue = Color(0xFF415A77)
 val Platinum = Color(0xFFE0E1DD)
 val CyanAqua = Color(0xFF00FFFF) // Warna aksen yang cerah
